110926fe630c822d825fe9b87bbfb3c48570a847
Restyled.io <commits@restyled.io>
2022-03-26 10:18:13 +0000

    Restyled by astyle

--- examples/example2.c
+++ examples/example2.c
@@ -413,115 +413,115 @@ int main(void) {
         }
 
         switch (obj.type) {
-            case CMP_TYPE_POSITIVE_FIXNUM:
-            case CMP_TYPE_UINT8:
-                printf("Unsigned Integer: %u\n", obj.as.u8);
-                break;
-            case CMP_TYPE_FIXMAP:
-            case CMP_TYPE_MAP16:
-            case CMP_TYPE_MAP32:
-                printf("Map: %u\n", obj.as.map_size);
-                break;
-            case CMP_TYPE_FIXARRAY:
-            case CMP_TYPE_ARRAY16:
-            case CMP_TYPE_ARRAY32:
-                printf("Array: %u\n", obj.as.array_size);
-                break;
-            case CMP_TYPE_FIXSTR:
-            case CMP_TYPE_STR8:
-            case CMP_TYPE_STR16:
-            case CMP_TYPE_STR32:
-                if (!read_bytes(sbuf, obj.as.str_size, fh)) {
+        case CMP_TYPE_POSITIVE_FIXNUM:
+        case CMP_TYPE_UINT8:
+            printf("Unsigned Integer: %u\n", obj.as.u8);
+            break;
+        case CMP_TYPE_FIXMAP:
+        case CMP_TYPE_MAP16:
+        case CMP_TYPE_MAP32:
+            printf("Map: %u\n", obj.as.map_size);
+            break;
+        case CMP_TYPE_FIXARRAY:
+        case CMP_TYPE_ARRAY16:
+        case CMP_TYPE_ARRAY32:
+            printf("Array: %u\n", obj.as.array_size);
+            break;
+        case CMP_TYPE_FIXSTR:
+        case CMP_TYPE_STR8:
+        case CMP_TYPE_STR16:
+        case CMP_TYPE_STR32:
+            if (!read_bytes(sbuf, obj.as.str_size, fh)) {
+                error_and_exit(strerror(errno));
+            }
+            sbuf[obj.as.str_size] = 0;
+            printf("String: %s\n", sbuf);
+            break;
+        case CMP_TYPE_BIN8:
+        case CMP_TYPE_BIN16:
+        case CMP_TYPE_BIN32:
+            memset(sbuf, 0, sizeof(sbuf));
+            if (!read_bytes(sbuf, obj.as.bin_size, fh)) {
+                error_and_exit(strerror(errno));
+            }
+            printf("Binary: %s\n", sbuf);
+            break;
+        case CMP_TYPE_NIL:
+            printf("NULL\n");
+            break;
+        case CMP_TYPE_BOOLEAN:
+            if (obj.as.boolean) {
+                printf("Boolean: true\n");
+            }
+            else {
+                printf("Boolean: false\n");
+            }
+            break;
+        case CMP_TYPE_EXT8:
+        case CMP_TYPE_EXT16:
+        case CMP_TYPE_EXT32:
+        case CMP_TYPE_FIXEXT1:
+        case CMP_TYPE_FIXEXT2:
+        case CMP_TYPE_FIXEXT4:
+        case CMP_TYPE_FIXEXT8:
+        case CMP_TYPE_FIXEXT16:
+            if (obj.as.ext.type == 1) { /* Date object */
+                if (!read_bytes(&year, sizeof(uint16_t), fh)) {
                     error_and_exit(strerror(errno));
                 }
-                sbuf[obj.as.str_size] = 0;
-                printf("String: %s\n", sbuf);
-                break;
-            case CMP_TYPE_BIN8:
-            case CMP_TYPE_BIN16:
-            case CMP_TYPE_BIN32:
-                memset(sbuf, 0, sizeof(sbuf));
-                if (!read_bytes(sbuf, obj.as.bin_size, fh)) {
+
+                if (!read_bytes(&month, sizeof(uint8_t), fh)) {
                     error_and_exit(strerror(errno));
                 }
-                printf("Binary: %s\n", sbuf);
-                break;
-            case CMP_TYPE_NIL:
-                printf("NULL\n");
-                break;
-            case CMP_TYPE_BOOLEAN:
-                if (obj.as.boolean) {
-                    printf("Boolean: true\n");
-                }
-                else {
-                    printf("Boolean: false\n");
-                }
-                break;
-            case CMP_TYPE_EXT8:
-            case CMP_TYPE_EXT16:
-            case CMP_TYPE_EXT32:
-            case CMP_TYPE_FIXEXT1:
-            case CMP_TYPE_FIXEXT2:
-            case CMP_TYPE_FIXEXT4:
-            case CMP_TYPE_FIXEXT8:
-            case CMP_TYPE_FIXEXT16:
-                if (obj.as.ext.type == 1) { /* Date object */
-                    if (!read_bytes(&year, sizeof(uint16_t), fh)) {
-                        error_and_exit(strerror(errno));
-                    }
 
-                    if (!read_bytes(&month, sizeof(uint8_t), fh)) {
-                        error_and_exit(strerror(errno));
-                    }
-
-                    if (!read_bytes(&day, sizeof(uint8_t), fh)) {
-                        error_and_exit(strerror(errno));
-                    }
-
-                    printf("Date: %u/%u/%u\n", year, month, day);
+                if (!read_bytes(&day, sizeof(uint8_t), fh)) {
+                    error_and_exit(strerror(errno));
                 }
-                else {
-                    printf("Extended type {%d, %u}: ",
-                        obj.as.ext.type, obj.as.ext.size
-                    );
-                    while (obj.as.ext.size--) {
-                        read_bytes(sbuf, sizeof(uint8_t), fh);
-                        printf("%02x ", sbuf[0]);
-                    }
-                    printf("\n");
+
+                printf("Date: %u/%u/%u\n", year, month, day);
+            }
+            else {
+                printf("Extended type {%d, %u}: ",
+                       obj.as.ext.type, obj.as.ext.size
+                      );
+                while (obj.as.ext.size--) {
+                    read_bytes(sbuf, sizeof(uint8_t), fh);
+                    printf("%02x ", sbuf[0]);
                 }
-                break;
-            case CMP_TYPE_FLOAT:
-                printf("Float: %f\n", obj.as.flt);
-                break;
-            case CMP_TYPE_DOUBLE:
-                printf("Double: %f\n", obj.as.dbl);
-                break;
-            case CMP_TYPE_UINT16:
-                printf("Unsigned Integer: %u\n", obj.as.u16);
-                break;
-            case CMP_TYPE_UINT32:
-                printf("Unsigned Integer: %u\n", obj.as.u32);
-                break;
-            case CMP_TYPE_UINT64:
-                printf("Unsigned Integer: %" PRIu64 "\n", obj.as.u64);
-                break;
-            case CMP_TYPE_NEGATIVE_FIXNUM:
-            case CMP_TYPE_SINT8:
-                printf("Signed Integer: %d\n", obj.as.s8);
-                break;
-            case CMP_TYPE_SINT16:
-                printf("Signed Integer: %d\n", obj.as.s16);
-                break;
-            case CMP_TYPE_SINT32:
-                printf("Signed Integer: %d\n", obj.as.s32);
-                break;
-            case CMP_TYPE_SINT64:
-                printf("Signed Integer: %" PRId64 "\n", obj.as.s64);
-                break;
-            default:
-                printf("Unrecognized object type %u\n", obj.type);
-                break;
+                printf("\n");
+            }
+            break;
+        case CMP_TYPE_FLOAT:
+            printf("Float: %f\n", obj.as.flt);
+            break;
+        case CMP_TYPE_DOUBLE:
+            printf("Double: %f\n", obj.as.dbl);
+            break;
+        case CMP_TYPE_UINT16:
+            printf("Unsigned Integer: %u\n", obj.as.u16);
+            break;
+        case CMP_TYPE_UINT32:
+            printf("Unsigned Integer: %u\n", obj.as.u32);
+            break;
+        case CMP_TYPE_UINT64:
+            printf("Unsigned Integer: %" PRIu64 "\n", obj.as.u64);
+            break;
+        case CMP_TYPE_NEGATIVE_FIXNUM:
+        case CMP_TYPE_SINT8:
+            printf("Signed Integer: %d\n", obj.as.s8);
+            break;
+        case CMP_TYPE_SINT16:
+            printf("Signed Integer: %d\n", obj.as.s16);
+            break;
+        case CMP_TYPE_SINT32:
+            printf("Signed Integer: %d\n", obj.as.s32);
+            break;
+        case CMP_TYPE_SINT64:
+            printf("Signed Integer: %" PRId64 "\n", obj.as.s64);
+            break;
+        default:
+            printf("Unrecognized object type %u\n", obj.type);
+            break;
         }
     }
 
